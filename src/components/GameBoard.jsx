52e778fd7f05c1cda17c492ad8ee34d3f697f41d
Mo Chahdoura <chahdoura.mohamed6@gmail.com>
2024-06-16 00:07:13 +0200

    v1.2

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const initialGameBoard =[
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+];
+
+export default function GameBoard({onSelectSquare, activePlayerSymbol}){
+    const[gameBoard, setGameBoard]= useState(initialGameBoard)
+    
+    function handelSelectedSquare(rowIndex, colIndex){
+        setGameBoard((previousGameBoard)=>{
+            const updatedGameBoard =[...previousGameBoard.map(innerArray => [...innerArray])];
+            updatedGameBoard[rowIndex][colIndex]=activePlayerSymbol;
+            return updatedGameBoard ;
+        });
+        onSelectSquare()
+    }
+
+    return (
+                <ol id="game-board">
+                    {gameBoard.map((row, rowIndex)=>(
+                        <li key ={rowIndex}>
+                            <ol>
+                                {row.map((playerSymbol, colIndex)=>(
+                                    <li key={colIndex}>
+                                        <button onClick={() => handelSelectedSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                                    </li>
+                                ))}
+                            </ol>
+                        </li>
+                    ))}
+                </ol>
+    );
+
+}
